5a7d50e8b14e552bad3ca3a8909b7a9e5590070a
pradiep kummar <pky463775@gmail.com>
2024-05-30 14:53:04 +0530

    some new

--- src/pages/api/auth/callback/google.ts
+++ src/pages/api/auth/callback/google.ts
@@ -1,15 +1,15 @@
-import prisma from "@/lib/prisma";
-import NextAuth from "next-auth/next";
+// import prisma from "@/lib/prisma";
+// import NextAuth from "next-auth";
 
-export default NextAuth({
-    callbacks:{
-        async jwt(token,user,account,profile,isNewuser){
-            if (isNewuser) {
-                const user=await prisma.user.create({
-                    name:
-                });
-            }
-            return token
-        }
-    }
-});
+// export default NextAuth({
+//     callbacks:{
+//         async jwt(token,user,account,profile,isNewuser){
+//             if (isNewuser) {
+//                 const user=await prisma.user.create({
+//                     name:
+//                 });
+//             }
+//             return token
+//         }
+//     }
+// });
